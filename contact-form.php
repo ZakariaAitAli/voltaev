fd1c2626f2c46bda526fb8c4aacfa024272f6d59
jaafaryassine <yass.jaafar0@gmail.com>
2022-10-20 18:30:40 +0000

    volta

---
+++ contact-form.php
@@ -0,0 +1,26 @@
+<?php 
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['message'])) {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $message = htmlspecialchars($_POST['message']);
+
+    define('email','voltaensa@gmail.com');
+    $to_email = "yass.jaafar0@gmail.com";
+    $subject = "Volta Contact";
+    $body = "<html>
+                Nom :".$name."<br> Email :".$email."<br> Phone :".$phone."<br> Message:".$message."<br>
+            </html>";
+ 
+if (mail($to_email, $subject, $body,'FROM: '.email))
+ 
+{
+    echo "Email successfully sent to $to_email...";
+}
+ 
+else
+ 
+{
+    echo "Email sending failed!";
+}
+}
